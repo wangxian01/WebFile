71a245b5e623b4e6c144a5f556a78099e4abf0b9
wangxian <2273566160@qq.com>
2020-04-08 16:53:04 +0800

    代码提交

--- app/src/main/java/com/yimiduo/webfile/MainActivity.java
+++ app/src/main/java/com/yimiduo/webfile/MainActivity.java
@@ -2,13 +2,114 @@ package com.yimiduo.webfile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+
+import com.tencent.smtt.sdk.ValueCallback;
+import com.tencent.smtt.sdk.WebChromeClient;
 
 public class MainActivity extends AppCompatActivity {
+    private MyWebView webView;
+
+    private ValueCallback<Uri> uploadFile;
+    private ValueCallback<Uri[]> uploadFiles;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE | WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+        init();
+        webView.loadUrl("https://i.csdn.net/#/uc/profile");
+    }
+
+    private void init(){
+        webView.setWebChromeClient(new WebChromeClient() {
+            // For Android 3.0+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType) {
+                MainActivity.this.uploadFile = uploadFile;
+                openFileChooseProcess();
+            }
+
+            // For Android < 3.0
+            public void openFileChooser(ValueCallback<Uri> uploadMsgs) {
+                MainActivity.this.uploadFile = uploadFile;
+                openFileChooseProcess();
+            }
+
+            // For Android  > 4.1.1
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                MainActivity.this.uploadFile = uploadFile;
+                openFileChooseProcess();
+            }
+
+            // For Android  >= 5.0
+            public boolean onShowFileChooser(com.tencent.smtt.sdk.WebView webView,
+                                             ValueCallback<Uri[]> filePathCallback,
+                                             WebChromeClient.FileChooserParams fileChooserParams) {
+                MainActivity.this.uploadFiles = filePathCallback;
+                openFileChooseProcess();
+                return true;
+            }
+
+        });
+    }
+
+    private void openFileChooseProcess() {
+        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+        i.addCategory(Intent.CATEGORY_OPENABLE);
+        i.setType("*/*");
+        startActivityForResult(Intent.createChooser(i, "test"), 0);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case 0:
+                    if (null != uploadFile) {
+                        Uri result = data == null ? null : data.getData();
+                        uploadFile.onReceiveValue(result);
+                        uploadFile = null;
+                    }
+                    if (null != uploadFiles) {
+                        Uri result = data == null  ? null : data.getData();
+                        uploadFiles.onReceiveValue(new Uri[]{result});
+                        uploadFiles = null;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        } else if (resultCode == RESULT_CANCELED) {
+            if (null != uploadFile) {
+                uploadFile.onReceiveValue(null);
+                uploadFile = null;
+            }
+            if (null != uploadFiles) {
+                uploadFiles.onReceiveValue(null);
+                uploadFiles = null;
+            }
+
+        }
+    }
+
+    /**
+     * 确保注销配置能够被释放
+     */
+    @Override
+    protected void onDestroy() {
+        if (this.webView != null) {
+            webView.destroy();
+        }
+        super.onDestroy();
     }
 }
